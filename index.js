854f655980e36a6bad995fa773f254f82842808e
Austin Barrington <arbarrington12@gmail.com>
2022-08-23 15:19:26 -0600

    commit tree be25508e920f4675bfe29144a981fb7bde3f1865

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+    let userObject = {name, email}
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(
+            userObject
+        )
+    })
+    .then(function(res)  
+        {return res.json();})
+    .then(function(object) 
+        {document.body.innerHTML = object['id'];})
+    .catch((err) => 
+        {document.body.innerHTML = err.message})
+}
+
+// submitData();
+// let userObject = {name, email}
